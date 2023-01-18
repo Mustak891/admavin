63570bae0729d1cc6a456289fc219c81aa2a3fab
FIRST_NAME LAST_NAME <mustakahammed891@gmail.com>
2023-01-18 14:22:26 +0530

    admavin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ElementTransfer from './components/element_transfer/ElementTransfer';
+import ButtonAppBar from './components/navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import NestedList from './components/Nested_list_component/NestedList';
+import Infinite from './components/InfiniteScroll/InfiniteScroll';
+import Game from './components/Game/Game';
+import BoxSplitter from './components/Box split/Boxsplit';
 
-function App() {
+const App = () => {
+
+  const items = [
+    {
+      id: 1,
+      name: "Parent 1",
+      children: [
+        {
+          id: 2,
+          name: "Child 1",
+          children: [
+            {
+              id: 3,
+              name: "Grandchild 1",
+            },
+            {
+              id: 4,
+              name: "Grandchild 2",
+            },
+          ],
+        },
+        {
+          id: 5,
+          name: "Child 2",
+        },
+      ],
+    },
+    {
+      id: 6,
+      name: "Parent 2",
+    },
+  ];
+
+  function handleClick(item) {
+    console.log(item);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ButtonAppBar />
+      <Routes>
+        <Route path="/" element={<ElementTransfer />} />
+        <Route path="/nestedlist" element={<NestedList items={items} onClick={handleClick} />} />
+        <Route path='/Infinitescroll' element={<Infinite />} />
+        <Route path='/game' element={<Game />} />
+        <Route path='/boxsplit' element={<BoxSplitter initialSize={500} />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
+
+
